e7d73930e8831c3cad9de0b10072b8c14afe2ac2
will <will.boyd@abwlabs.com>
2020-04-21 15:49:48 -0500

    Added blink to startup

--- button-plus-led/main.py
+++ button-plus-led/main.py
@@ -1,5 +1,6 @@
 import argparse
 from gpiozero import LED, Button
+from time import sleep
 
 
 def main():
@@ -9,6 +10,13 @@ def main():
     parser = argparse.ArgumentParser(description='Turn an LED on when a button is pressed')
     parser.add_argument('--led', '-l', type=str, default="GPIO17", help='The pin the LED is connected to')
     parser.add_argument('--button', '-b', type=str, default="GPIO27", help='The pin the button is connected to')
+    parser.add_argument(
+        '--blink-when-ready',
+        default=False,
+        dest='blink',
+        action='store_true',
+        help='If set, blink the LED when GPIO is ready'
+    )
 
     args = parser.parse_args()
 
@@ -16,6 +24,13 @@ def main():
     button = Button(args.button)
 
     print('Ready for interaction!')
+
+    if args.blink:
+        for _ in range(1, 5):
+            led.on()
+            sleep(0.5)
+            led.off()
+            sleep(0.5)
 
     while True:
         button.wait_for_press()
